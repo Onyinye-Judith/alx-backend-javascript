5440013d81f1b22686c5f1e0ffe396624a11259c
Onyinye-Judith <onyinyejudith55@yahoo.com>
2024-11-29 20:32:09 +0100

    Unittests

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -1,36 +1,22 @@
 const express = require('express');
-
 const app = express();
-const PORT = 7865;
+const bodyParser = require('body-parser');
 
-app.use(express.json());
+app.use(bodyParser.json());
+... (previous endpoints)
 
-app.get('/', (_req, res) => {
-	  res.send('Welcome to the payment system');
+GET /available_payments
+app.get('/available_payments', (req, res) => {
+	const paymentMethods = {
+		credit_cards: true,
+		paypal: false
+
+	};
+	res.json({ payment_methods: paymentMethods });
 });
-
-app.get('/cart/:id(\\d+)', (req, res) => {
-	  const id = req.params.id;
-
-	  res.send(`Payment methods for cart ${id}`);
-});
-
-app.get('/available_payments', (_req, res) => {
-	  res.json({ payment_methods: { credit_cards: true, paypal: false } });
-});
-
+POST /login
 app.post('/login', (req, res) => {
-	  let username = '';
-
-	  if (req.body) {
-		      username = req.body.userName;
-		    }
-
-	  res.send(`Welcome ${username}`);
+	const { userName } = req.body;
+	res.send(`Welcome ${userName}`);
 });
-
-app.listen(PORT, () => {
-	  console.log(`API available on localhost port ${PORT}`);
-});
-
 module.exports = app;
